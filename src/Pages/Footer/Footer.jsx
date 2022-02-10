450bcd5ae429f6a32146b8d2d30e192ba066fdef
Tosin Rotimidokun <rotimidokun@gmail.com>
2022-02-10 09:06:18 +0100

    Built for my girlfriend

---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p>Always loving ❤ you &#169; Ayomiposi</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import LoveIcon from '../../assets/combined-love.svg'
+import Line from '../../assets/whiteline.svg'
+import Ife1 from '../../assets/1.png'
+import Ife2 from '../../assets/2.png'
+import Ife3 from '../../assets/3.png'
+import Ife4 from '../../assets/4.png'
+import Ife5 from '../../assets/5.png'
+import Ife6 from '../../assets/6.png'
+import Ife7 from '../../assets/7.png'
+import Ife8 from '../../assets/8.png'
+import Ife9 from '../../assets/9.png'
+import Ife10 from '../../assets/10.png'
+import Ife11 from '../../assets/11.png'
+import Ife12 from '../../assets/12.png'
+import Ife13 from '../../assets/13.png'
+import Ife14 from '../../assets/14.png'
+import Ife15 from '../../assets/15.png'
+
+
+import './home.css'
+
+const Home = () => {
+    return (
+        <>
+            <div className="HomeWrapper">
+
+                <div>
+                    <div>
+                        <div className="TopWrapper">
+                            <p>6 years of friendship</p>
+                            <img src={LoveIcon} alt="" />
+                        </div>
+
+                        {/* <img src={Line} alt="" className="Line" /> */}
+                    </div>
+
+                    <div className="SecondSection">
+                        <p>Always Loving You</p>
+                        <h1>Oluwatosin <br />
+                            Charis</h1>
+                    </div>
+                </div>
+
+                <div className="rightSection">
+                    <p>" I always wonder how I got so lucky to have you in my life. The beauty, Your love for God, Intelligence, Humitlity.  
+                    <span style={{ color: 'red'}}> I love you.</span> "</p>
+
+                    <div className="Images">
+                        <img src={Ife1} alt="" height="330px" />
+                        <img src={Ife2} alt="" height="330px" />
+                        <img src={Ife3} alt="" height="330px" />
+                        <img src={Ife4} alt="" height="330px" />
+                        <img src={Ife5} alt="" height="330px" />
+                        <img src={Ife6} alt="" height="330px" />
+                        <img src={Ife7} alt="" height="330px" />
+                        <img src={Ife8} alt="" height="330px" />
+                        <img src={Ife9} alt="" height="330px" />
+                        <img src={Ife10} alt="" height="330px" />
+                        <img src={Ife11} alt="" height="330px" />
+                        <img src={Ife12} alt="" height="330px" />
+                        <img src={Ife13} alt="" height="330px" />
+                        <img src={Ife14} alt="" height="330px" />
+                        <img src={Ife15} alt="" height="330px" />
+                    </div>
+
+                    <p className="Footer">Always loving ❤ you &#169; Ayomiposi</p>
+                </div>
+
+
+                <div className="LoveListSection">
+                    <h1 className="Age">27</h1>
+                    <p className="AgeSubText">Things I love about you</p>
+
+                    <div className="LoveList">
+                        <p> 1. You are the Most Beautiful Girl in the World</p>
+                        <p>2. You are Tall </p>
+                        <p>3. Your kindness is so rare</p>
+                        <p>4. You are so Intelligent, Sensei 🙌</p>
+                        <p>5. Your skin like chocolate 🍫 </p>
+                        <p>6. You mesmerize me</p>
+                        <p>7. Your laughter should be on replay</p>
+                        <p>8. You are a woman of God</p>
+                        <p>9. You are full of the spirit of God</p>
+                        <p>10. Your money is plenty, wealth!!!</p>
+                        <p>11. Yet, still so humble...</p>
+                        <p>12. Romantic: I dey learn romance from you.</p>
+                        <p>13. You have a beautiful smile</p>
+                        <p>14. You are so sexy, c'mon nau!!</p>
+                        <p>15. You bear my name, I bear your name 😁</p>
+                        <p>16. We have amazing mutual friends</p>
+                        <p>17. You are very teachable </p>
+                        <p>18. You always want the best for me</p>
+                        <p>19. You don't have one favorite color, so keep me confused 😁</p>
+                        <p>20. You are passionate about Education</p>
+                        <p>21. You are passionate about your Friends</p>
+                        <p>22. You want to join the iPhone gang. Lol</p>
+                        <p>23. For the special name you gave me.</p>
+                        <p>24. For the cars you will buy for me 😂</p>
+                        <p>25. We are going to have an amazing union</p>
+                        <p>26. You forgive me </p>
+                        <p>27. I feel overflowing peace with you</p>
+                        <br />
+                        <p> And... My friend of 6 years</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="ForMobile">
+                <h1>Please view on a desktop</h1>
+            </div>
+
+        </>
+
+    )
+}
+
+export default Home
